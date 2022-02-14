ef3c185c86268eca34f1f485f3af39c1a9dc03d2
Lapina-SL <svetlanka5160@gmail.com>
2022-02-14 10:41:28 +0300

    edit write

--- lab04a/filter/build.sbt
+++ lab04a/filter/build.sbt
@@ -4,7 +4,6 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.11.12",
   libraryDependencies += "org.apache.spark" %%  "spark-core" % "2.4.6",
   libraryDependencies += "org.apache.spark" %%  "spark-sql" % "2.4.6",
-  libraryDependencies += "io.circe" %% "circe-parser" % "0.9.0"
 )
 
 lazy val root = (project in file(".")).
--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -18,12 +18,14 @@ object filter {
       "startingOffsets" -> spark.conf.get("spark.filter.offset")
     )
 
-    val sdf = spark.readStream
+    val df = spark.read
       .format("kafka")
       .options(kafkaParams)
+      .option("checkpointLocation", "/user/svetlana.lapina/tmp/chk")
       .load
 
-    val schema =  new StructType()
+
+    val schema = new StructType()
       .add("event_type", StringType, true)
       .add("category", StringType, true)
       .add("item_id", StringType, true)
@@ -31,53 +33,31 @@ object filter {
       .add("uid", StringType, true)
       .add("timestamp", LongType, true)
 
-    val parsedSdf = sdf.select(from_json('value.cast("string"), schema).alias("value"))
-      .select(col("value.event_type").alias("event_type"),
-        col("value.category").alias("category"),
+    val parsedDf = df.select(from_json('value.cast("string"), schema).alias("value"))
+      .select(col("value.category").alias("category"),
+        col("value.event_type").alias("event_type"),
         col("value.item_id").alias("item_id"),
         col("value.item_price").alias("item_price"),
-        col("value.uid").alias("uid"),
-        col("value.timestamp").alias("timestamp")
+        col("value.timestamp").alias("timestamp"),
+        col("value.uid").alias("uid")
       )
       .withColumn("date", from_unixtime(col("timestamp") / 1000, "yyyyMMdd"))
       .withColumn("p_date", col("date"))
+      .persist()
 
-    val viewDf = parsedSdf.filter(col("event_type") === lit("view"))
-    val buyDf = parsedSdf.filter(col("event_type") === lit("buy"))
+    val viewDf = parsedDf.filter(col("event_type") === lit("view"))
+    val buyDf = parsedDf.filter(col("event_type") === lit("buy"))
 
-    val sink1 = createViewSink(viewDf)
-    val sink2 = createBuySink(buyDf)
-
-    val sq1 = sink1.start
-    val sq2 = sink2.start
-
-    sq1.awaitTermination()
-    sq2.awaitTermination()
-  }
-
-  def createConsoleSink(df: DataFrame): DataStreamWriter[Row] = {
-    df.writeStream
-      //.partitionBy("p_event_type", "p_date")
-      .format("console")
-      .trigger(Trigger.ProcessingTime("10 seconds"))
-      .foreachBatch((batch, id) => batch.show(20, false))
-  }
-
-  def createViewSink(df: DataFrame): DataStreamWriter[Row] = {
-    df.writeStream
+    viewDf.write
       .partitionBy("p_date")
-      .format("parquet")
-      .trigger(Trigger.ProcessingTime("5 seconds"))
-      .option("checkpointLocation", "/user/svetlana.lapina/tmp/chk/view")
-      .option("path", spark.conf.get("spark.filter.output_dir_prefix")+ "/view")
+      .json(spark.conf.get("spark.filter.output_dir_prefix") + "/view")
+
+    buyDf.write
+      .partitionBy("p_date")
+      .json(spark.conf.get("spark.filter.output_dir_prefix") + "/buy")
+
+    parsedDf.unpersist()
+    spark.close()
   }
 
-  def createBuySink(df: DataFrame): DataStreamWriter[Row] = {
-    df.writeStream
-      .partitionBy("p_date")
-      .format("parquet")
-      .trigger(Trigger.ProcessingTime("5 seconds"))
-      .option("checkpointLocation", "/user/svetlana.lapina/tmp/chk/buy")
-      .option("path", spark.conf.get("spark.filter.output_dir_prefix")+ "/buy")
-  }
 }
